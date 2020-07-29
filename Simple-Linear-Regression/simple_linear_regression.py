0dec38442f382ee826a1fbb2a71b73c05fc4e234
Precious Ibeagi <preciousaang@gmail.com>
2020-07-29 15:50:06 +0100

    Simple Linear Regression

---
+++ Simple-Linear-Regression/simple_linear_regression.py
@@ -0,0 +1,59 @@
+#Import the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+'''
+# Encode Categgorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_X.fit_transform(y)
+
+'''
+
+#Splitting dataset into training set and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
+
+'''
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+'''
+
+# Fit the simple linear regression to the training set 
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the test set results
+y_pred = regressor.predict(X_test)
+
+# Visualising the training set result
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience (Training Set)')
+plt.xlabel('Years Of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Visualising the test set result
+plt.scatter(X_test, y_test, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience (Test Set)')
+plt.xlabel('Years Of Experience')
+plt.ylabel('Salary')
+plt.show()
+
